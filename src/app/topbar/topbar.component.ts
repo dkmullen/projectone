ea677716877c79e164a38aeddc888645a0a94bdb
Dennis Mullen <dkmullen@gmail.com>
2018-09-03 11:18:27 -0400

    use queryselector instead of getlelem etc

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -11,12 +11,14 @@ export class TopbarComponent {
   public theme: any;
   public currentTheme: any;
 
-  changeTheme() {
-    this.currentTheme = document.getElementById('body').classList;
+  changeTheme() { 
+    let body = document.querySelector('body');
+    this.currentTheme = body.classList;
     this.theme = this.currentTheme[0]
+
     this.theme === 'kurtz-theme' ? this.theme = 'kurtz-dark-theme' : this.theme = 'kurtz-theme';
-    document.getElementById('body').classList.remove(this.currentTheme);
-    document.getElementById('body').classList.add(this.theme);      
+    body.classList.remove(this.currentTheme);
+    body.classList.add(this.theme);      
     localStorage.setItem('currentTheme', this.theme);
   }
 }
