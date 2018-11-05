189a7f638d1eb751ad34f4c96fc1da6bc50ae19a
Dennis Mullen <dkmullen@gmail.com>
2018-11-04 20:33:12 -0500

    random select article

--- src/app/button-bar/button-bar.component.ts
+++ src/app/button-bar/button-bar.component.ts
@@ -9,14 +9,20 @@ import { ArticleListService } from '../services/articlelist.service';
   styleUrls: ['./button-bar.component.css']
 })
 export class ButtonBarComponent implements OnInit {
+  public articles: any;
 
   constructor(protected router: Router, public articleList: ArticleListService) { }
 
   ngOnInit() {
+    this.articles = this.articleList.articles;
   }
 
   randomArticle() {
-    console.log('random')
+    // Pick a number between 1 and the list length minus one (don't want 0, the intro page)
+    let min = 1;
+    let max = this.articles.length - 1;
+    let n = Math.floor(Math.random() * max + min);
+    this.router.navigate(new Array('/').concat([this.articles[n]])); 
   }
   
   pageOne() {
